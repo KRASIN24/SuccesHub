865968a131281360acd177244c1c0ba60bdaf1e7
KRASIN24 <jakub.777@outlook.com>
2026-02-22 01:18:52 +0100

    Initial commit

---
+++ src/main/java/com/succeshub/succes_hub/SuccesHubApplication.java
@@ -0,0 +1,13 @@
+package com.succeshub.succes_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuccesHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuccesHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/succeshub/succes_hub/SuccesHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.succeshub.succes_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuccesHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
