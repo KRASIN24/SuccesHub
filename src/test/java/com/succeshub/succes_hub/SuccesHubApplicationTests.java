5056bd4e16cc9cd0f2a5a2ac859a8b765d9d5ef9
KRASIN24 <jakub.777@outlook.com>
2026-06-10 19:09:22 +0200

    App test

--- src/test/java/com/succeshub/succes_hub/SuccesHubApplicationTests.java
+++ src/test/java/com/succeshub/succes_hub/SuccesHubApplicationTests.java
@@ -1,13 +1,43 @@
 package com.succeshub.succes_hub;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=" +
+				"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+				"org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration",
+		"spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:9999/fake-jwks"
+})
+@AutoConfigureMockMvc
 class SuccesHubApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void swaggerUiIsPublic() throws Exception {
+		mockMvc.perform(get("/swagger-ui/index.html"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void openApiDocsArePublicAndCorrect() throws Exception {
+		mockMvc.perform(get("/v3/api-docs"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.openapi").exists())
+				.andExpect(jsonPath("$.info.title").value("SuccessHub API"));
+	}
 }
