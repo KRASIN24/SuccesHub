e6042f87fea2da7044d3843ee6fc8da664e95c21
KRASIN24 <jakub.777@outlook.com>
2026-06-18 19:32:36 +0200

    Bug fixing and improvements

--- src/main/java/com/succeshub/succes_hub/SuccesHubApplication.java
+++ src/main/java/com/succeshub/succes_hub/SuccesHubApplication.java
@@ -3,6 +3,10 @@ package com.succeshub.succes_hub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot entry point. Component scan covers appdomain services, controllers,
+ * and {@link com.succeshub.config.JpaConfig} for JPA repository registration.
+ */
 @SpringBootApplication(scanBasePackages = {
 		"com.succeshub.succes_hub",
 		"com.succeshub.config",
